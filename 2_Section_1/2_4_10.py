9c29e5db3652585aeef72c6e9e79d21e226f654e
rojas <rojas.rj@gmail.com>
2025-08-11 20:28:45 -0600

    Another commit

---
+++ 2_Section_1/2_4_10.py
@@ -0,0 +1,17 @@
+x =  0
+x = float(x)
+# 3x3 - 2x2 + 3x - 1
+y = 3 * x*x*x - 2 * x*x + 3 * x - 1
+print("y =", y)
+
+x1 =  1
+x1 = float(x1)
+# 3x3 - 2x2 + 3x - 1
+y1 = 3 * x1*x1*x1 - 2 * x1*x1 + 3 * x1 - 1
+print("y1 =", y1)
+
+x2 =  -1
+x2 = float(x2)
+# 3x3 - 2x2 + 3x - 1
+y2 = 3 * x2*x2*x2 - 2 * x2*x2 + 3 * x2 - 1
+print("y2 =", y2)
---
+++ 2_Section_1/2_4_7.py
@@ -0,0 +1,5 @@
+john, mary, adam = 3, 2, 1
+print(john,",", mary,",",adam)
+total_apples = john + mary + adam
+print("Total number of apples:", total_apples)
+
